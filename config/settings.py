7c4d82d9e2ea1334e5a660afa49bfca40a1240df
ooguroiori <iori.oguro@g.dreamcareer.co.jp>
2025-02-08 15:59:27 +0900

    feat: プロキシの追加

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ YTDL_OPTIONS = {
     # 地域とネットワーク設定
     'geo_bypass': True,                 # 地域制限をバイパス
     'geo_bypass_country': 'JP',         # 日本のIPをシミュレート
-    'proxy': '',                        # 必要に応じてプロキシを設定可能
+    'proxy': 'socks5://127.0.0.1:40000',# 必要に応じてプロキシを設定可能
     'force-ipv4': True,                 # IPv4の使用を強制
     'source_address': '0.0.0.0',        # 接続元IPアドレス
     
